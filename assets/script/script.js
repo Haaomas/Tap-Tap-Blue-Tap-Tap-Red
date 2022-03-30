7345c896b75d28905a004595ee20e2d8787f44aa
Haaomas <barreauji@gmail.com>
2022-03-30 22:33:28 +0200

    First Step
    CSS in good way
    JS is planned
    Happy Me

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+//Test de connexion
+console.log("Be water my Friend");
+
+//Début du code
+/*
+ * PSEUDO CODE 
+ * Relier chaque div à une touche du clavier
+ * (Possibilité de mettre les div à 0 dans le CSS et de les chargé dans le JS) 
+ * Au click ajouter +5 de dimension à la div et retirer -5 à l'adversaire
+ * If div = 0 alert victoire des bleus ou rouge  
+ */
