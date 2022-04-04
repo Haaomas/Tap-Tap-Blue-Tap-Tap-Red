09dbe7b2307681662855b6736649151625fc93e8
Haaomas <barreauji@gmail.com>
2022-04-04 19:14:01 +0200

    Tap Tap Good
    Function With ++ ✔️

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,28 @@
 //Test de connexion
 console.log("Be water my Friend");
 
-//Début du code
-/*
- * PSEUDO CODE 
- * Relier chaque div à une touche du clavier
- * (Possibilité de mettre les div à 0 dans le CSS et de les chargé dans le JS) 
- * Au click ajouter +5 de dimension à la div et retirer -5 à l'adversaire
- * If div = 0 alert victoire des bleus ou rouge  
- */
+//Beginning of the project
+//Blue side 
+const BLUE_SIDE = document.getElementById("blue");
+
+//Initial width
+let blueWidth = 50;
+
+//Event listener
+document.addEventListener("keydown", fight);
+
+//Function
+function fight(e) {
+    let keyClick = e.key;
+    console.log('keyClick', keyClick)
+    switch (keyClick) {
+        case "q":
+            blueWidth = blueWidth + 2.5;
+            BLUE_SIDE.style.setProperty("--blue-width", blueWidth + "%");
+            break;
+        case "m":
+            blueWidth = blueWidth - 2.5;
+            BLUE_SIDE.style.setProperty("--blue-width", blueWidth + "%");
+            break;
+    }
+}
