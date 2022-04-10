8e7b02e937c0d52320f5db980427066c97f42970
Haaomas <barreauji@gmail.com>
2022-04-10 16:54:33 +0200

    Victory condition Good
    Blue team or red Team Win ok
    Need to do the CSS Now

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,6 +5,9 @@ console.log("-----")
 //Beginning of the project
 //Blue side 
 const BLUE_SIDE = document.getElementById("blue");
+const RED_SIDE = document.getElementById("gameFieldRed");
+const RED_SIDE_STYLE = getComputedStyle(RED_SIDE).width
+let blueSideStyle = ""
 
 //Initial width
 let blueWidth = 50;
@@ -16,13 +19,31 @@ document.addEventListener("keydown", fight);
 function fight(e) {
     let keyClick = e.key;
     switch (keyClick) {
+        //Blue Side 
         case "q":
+            //Add Width +2.5
             blueWidth = blueWidth + 2.5;
             BLUE_SIDE.style.setProperty("--blue-width", blueWidth + "%");
+            //Get de With for the win 
+            blueSideStyle = getComputedStyle(BLUE_SIDE).width;
+            console.log('blueSideStyle', blueSideStyle)
+            console.log('RED_SIDE_STYLE', RED_SIDE_STYLE)
             break;
+        //RED Side     
         case "m":
+            //Add Width +2.5
             blueWidth = blueWidth - 2.5;
             BLUE_SIDE.style.setProperty("--blue-width", blueWidth + "%");
+            //Get de With for the win 
+            blueSideStyle = getComputedStyle(BLUE_SIDE).width;
+            console.log('blueSideStyle', blueSideStyle)
+            console.log('RED_SIDE_STYLE', RED_SIDE_STYLE)
             break;
+    }
+    //Victory condition
+    if (blueSideStyle == "0px") {
+        console.log("Red Team Win")
+    } else if (blueSideStyle === RED_SIDE_STYLE) {
+        console.log("Blue Team Win")
     }
 }
